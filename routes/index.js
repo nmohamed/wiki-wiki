9d82b9cfe6f1aed8e1f41961f5395eac9fc80cb0
Nora Mohamed <jurukix@gmail.com>
2016-02-26 16:44:16 -0500

    fixin home

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,8 @@
-// var Items = require('../models/itemModel.js');
 var mongoose = require('mongoose');
 var path = require('path');
 
-/* HOME */
-
 var home = function(req, res){
-	res.sendFile('index.html', { root: path.join(__dirname, '../public') });
+	res.end();
 };
 
 module.exports.home = home;
-
-// /* ADD */
-
-// var add = function(req, res){
-// 	res.render('remove', {message: 'removed'});
-// };
-
-// module.exports.add = add;
-
-// /* REMOVE */
-
-// var remove = function(req, res){
-// 	res.render('remove', {message: 'removed'});
-// };
-
-// module.exports.remove = remove;

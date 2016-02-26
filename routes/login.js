cb02f6d258c7787da0384f8571a51c334064dae7
Nora Mohamed <jurukix@gmail.com>
2016-02-26 16:42:28 -0500

    adding routes

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,15 @@
-// var Items = require('../models/itemModel.js');
 var mongoose = require('mongoose');
 var path = require('path');
 
-var loginGET = function(req, res){
+var GETlogin = function(req, res){
 	res.end();
 };
 
-module.exports.loginGET = loginGET;
+module.exports.GETlogin = GETlogin;
 
-var loginPOST = function(req, res){
+var POSTlogin = function(req, res){
 	res.json({message:'Login successful'});
 	//res.json({message:'Login unsuccessful'});
 };
 
-module.exports.loginPOST = loginPOST;
+module.exports.POSTlogin = POSTlogin;
---
+++ routes/pages.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var path = require('path');
+
+var GETallPages = function(req, res){
+	res.end();
+};
+
+module.exports.GETallPages = GETallPages;
+
+var GETpage = function(req, res){
+	res.end();
+};
+
+module.exports.GETpage = GETpage;
+
+var POSTsubmit = function(req, res){
+	res.end();
+};
+
+module.exports.POSTsubmit = POSTsubmit;
+
+var POSTedit = function(req, res){
+	res.end();
+};
+
+module.exports.POSTedit = POSTedit;
+
+var DELETEpage = function(req, res){
+	res.end();
+};
+
+module.exports.DELETEpage = DELETEpage;
